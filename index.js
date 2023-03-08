843846ade20069937764ad115811d2b9ce6bfdfa
nyamvularinga <nyamvularinga@gmail.com>
2023-03-08 14:24:38 +0300

    Test 2

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function introduction(name){
-    console.log("Hi, my name is ${name}");
-    return 'Hi, my name is ${name}';
+function introduction(name = "Aki"){
+    return('Hi, my name is ${name}.');
 };
+introduction();
+
+function introductionWithLanguage(name="Aki" , language="Ember.js"){
+    return('Hi, my name is ${name} and I am learning to program in ${language}.');
+
+};
+introductionWithLanguage();
+
+function introductionWithLanguageOptional(name="Aki" ,language="JavaScript"){
+    return('Hi, my name is ${name} and I am learning to program in ${language}.');
+
+};
+introductionWithLanguageOptional();
